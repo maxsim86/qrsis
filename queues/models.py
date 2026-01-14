4d5dc5bb60f4b860dca30c6973f856346742622e
sairi <sairi@debian>
2026-01-14 14:17:15 +0800

    fix friendly text

--- queues/models.py
+++ queues/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 import uuid
+from django.utils.text import slugify
 # Create your models here.
 
 class Queue(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, default=uuid.uuid4) # Unik link untuk QR
+    name = models.CharField(max_length=200, verbose_name="Nama Barisan")
+    #name = models.CharField(max_length=100)
+    #slug = models.SlugField(unique=True, default=uuid.uuid4) # Unik link untuk QR
+    slug = models.SlugField(unique=True, max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     # Untuk simpan input label (contoh: "Masukkan nama anda")
     input_label = models.CharField(max_length=100, default="Masukkan nama anda")
@@ -25,6 +28,27 @@ class Queue(models.Model):
         ('MS', 'Malay'),
     ]
     status_language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, default='AUTO')
+
+    def save(self, *args, **kwargs):
+        # Logic: Jika slug kosong, kita buat baru dari Nama
+        if not self.slug:
+            # Tukar nama jadi slug (contoh: "Klinik Dr. Ali" -> "klinik-dr-ali")
+            base_slug = slugify(self.name)
+            
+            # Jika slugify gagal (contoh nama simbol pelik), guna UUID pendek
+            if not base_slug:
+                base_slug = str(uuid.uuid4())[:8]
+
+            # Logic Unik: Pastikan tak ada slug yang sama
+            slug = base_slug
+            counter = 1
+            while Queue.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            
+            self.slug = slug
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
